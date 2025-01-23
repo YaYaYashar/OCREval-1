02826646b502a64f36695fa69bdb95aebb22c63a
Yashar Hsynlu <YasharHsynlu@gmail.com>
2025-01-23 17:59:50 +0330

    add setup files for Shotor and Arshasb datasets

---
+++ data/Arshasb/setup.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FILE='Arshasb_7k.tar.gz'
+LINK='https://drive.usercontent.google.com/download?id=1G9JEZY9MSzaND8ynnFodIXQvMMM1_6J3&export=download&authuser=0&confirm=t&uuid=b54052f6-2f0a-4d08-9eae-7c55d6e015ae&at=AIrpjvMMzg790Ax5fLRz3hmZ33B-%3A1737638100634'
+[ -f "$FILE" ] || wget -O "$FILE" "$LINK"
+[ -f Shotor_Images/0.tif ] || tar -xf "$FILE" &
---
+++ data/Shotor/setup.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+FILE='Shotor_Images.tar.gz'
+LINK='https://github.com/amirabbasasadi/Shotor/raw/refs/heads/master/Shotor_Images.tar.gz'
+[ -f "$FILE" ] || wget -O "$FILE" "$LINK"
+[ -f Shotor_Images/0.tif ] || tar -xf "$FILE" &
+
+
+FILE='Shotor_Words.csv'
+LINK='https://github.com/amirabbasasadi/Shotor/raw/refs/heads/master/Shotor_Words.csv'
+[ -f "$FILE" ] || wget -O "$FILE" "$LINK"
