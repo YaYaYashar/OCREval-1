2622685f1443a9ced16d8e7c4b4b91b2af7dc973
Yashar Hsynlu <YasharHsynlu@gmail.com>
2025-01-23 18:32:27 +0330

    fix shebang

--- data/Arshasb/setup.bash
+++ data/Arshasb/setup.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 FILE='Arshasb_7k.tar.gz'
 LINK='https://drive.usercontent.google.com/download?id=1G9JEZY9MSzaND8ynnFodIXQvMMM1_6J3&export=download&authuser=0&confirm=t&uuid=b54052f6-2f0a-4d08-9eae-7c55d6e015ae&at=AIrpjvMMzg790Ax5fLRz3hmZ33B-%3A1737638100634'
